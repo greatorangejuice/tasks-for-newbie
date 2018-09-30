4539b1ebfc6c25dc994287521fcc8c7d15732c8c
greatorangejuice <bypavelsnigirev@gmail.com>
2018-09-30 13:02:36 +0300

    New task from codewars

--- CW1.js
+++ CW1.js
@@ -1,11 +1,26 @@
-function testGit(count, access)
+function iqTest(numbers)
 {
-    while (access = true){
-        count++;
-        if (count > 99) break;
+  var str = numbers;
+  var even = 0;
+  var odd = 0;
+  var countEven, countOdd;
+  console.log(str.length)
+  for ( var i = 0; i < str.length; i++){
+    if (str[i] % 2 == 0){
+      even += 1;
+      countEven = i;
+    } 
+    else {
+      odd +=1;
+      countOdd = i;
     }
-    console.log(count);
+  }
+  if (even < odd){
+    return countEven;
+  }
+  else{
+    return countOdd;
+  }
+
 }
-testGit(10,true)
-alert(testGit(10,true));
-alert("Hello");
+alert(iqTest("2 4 7 8 10")); // прога не правильно считывает цифры
