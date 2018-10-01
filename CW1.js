eea3fa58722e256dc158eafbe8a0ac2ad03543f3
greatorangejuice <bypavelsnigirev@gmail.com>
2018-10-01 22:14:19 +0300

    Task is done

--- CW1.js
+++ CW1.js
@@ -1,20 +1,21 @@
 function iqTest(numbers)
 {
-  var str = numbers;
+  var str = numbers.split(" ");
   var even = 0;
   var odd = 0;
   var countEven, countOdd;
-  console.log(str.length)
+  console.log(str.length);
   for ( var i = 0; i < str.length; i++){
     if (str[i] % 2 == 0){
       even += 1;
-      countEven = i;
+      countEven = i+1;
     } 
     else {
       odd +=1;
-      countOdd = i;
+      countOdd = i+1;
     }
   }
+  
   if (even < odd){
     return countEven;
   }
@@ -23,8 +24,4 @@ function iqTest(numbers)
   }
 
 }
-alert(iqTest("2 4 7 8 10")); // прога не правильно считывает цифры
-
-// Найти способ выделять отдельно числа, где-то был способ
-// разделить строки
-// test
+alert(iqTest("2 4 7 8 10"));
