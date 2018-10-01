9a24e09e59f82ab7ff428c1a16f83389eaac1524
greatorangejuice <bypavelsnigirev@gmail.com>
2018-10-01 22:24:32 +0300

    upData

---
+++ CW2.js
@@ -0,0 +1,26 @@
+function getCount(str) {
+    var vowelsCount = 0;
+    
+    // enter your majic here
+    
+    return vowelsCount;
+  }
+
+
+  /*
+  Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, and u as vowels for this Kata.
+
+The input string will only consist of lower case letters
+and/or spaces. 
+  */
+
+  /////TEST/////
+/*
+ describe("Case 1", function(){
+    it ("should be defined", function(){
+        Test.assertEquals(getCount("abracadabra"), 5)
+    });
+});
+*/
