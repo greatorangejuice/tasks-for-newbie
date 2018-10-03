7bc5e55cfda56380cb82d714e26ccfbbfdc617dd
Pavel Snigirev <43197939+greatorangejuice@users.noreply.github.com>
2018-10-03 10:15:46 +0300

    Task is done

--- CW4.js
+++ CW4.js
@@ -1,3 +1,17 @@
-function tribonacci(signature,n){
-    //your code here
+function tribonacci(signature,n) {
+  var a = signature[0];
+  var b = signature[1];
+  var c = signature[2];
+  var e = a + b + c;   // I used letters because they are not constantly
+  var array = [a, b, c, e];
+  
+  for (var i = 0; i < n; i++) {
+    a = b;
+    b = c;
+    c = e;
+    e = a + b + c;
+    array.push(e);
   }
+  array = array.slice(0,n);
+  return array;
+}
