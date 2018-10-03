cc07a12f30dd40afb756fc71f0f4357ff6eebff0
greatorangejuice <bypavelsnigirev@gmail.com>
2018-10-03 23:14:00 +0300

    test

---
+++ test.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+// до вызова
+var menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+  function multiplyNumeric(obj){
+      for (var key in obj){
+
+      }
+  }
+  
+  multiplyNumeric(menu);
+  
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n)
+}
