e32e2d6d6ff3fefdb01c7c28b3c312e88d426e17
greatorangejuice <bypavelsnigirev@gmail.com>
2018-10-10 22:14:33 +0300

    new Task

---
+++ CW8.js
@@ -0,0 +1,17 @@
+var arr = [0, 3, 4, 5]
+
+// для каждого элемента массива запустить функцию,
+// промежуточный результат передавать первым аргументом далее
+var result = arr.reduce(function(sum, current) {
+  return sum*sum + current;
+}, 0);
+
+alert( result )
+
+
+
+
+function squareSum(numbers){
+    return 
+    }
+
