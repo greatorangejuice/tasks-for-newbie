a2230716306db6f9ee7f18b61df4f9fca2f0effa
greatorangejuice <bypavelsnigirev@gmail.com>
2018-10-14 19:04:27 +0300

    Task is done

--- CW8.js
+++ CW8.js
@@ -1,17 +1,25 @@
-var arr = [0, 3, 4, 5]
+var arr = [1, 2, 2]
 
 // для каждого элемента массива запустить функцию,
 // промежуточный результат передавать первым аргументом далее
-var result = arr.reduce(function(sum, current) {
-  return sum*sum + current;
-}, 0);
+// var result = arr.reduce(function(sum, current) {
+//   return sum*sum + current;
+// }, 0);
 
-alert( result )
-
-
+// alert( result )
 
 
-function squareSum(numbers){
-    return 
-    }
+
+
+function squareSum(numbers) {
+  var sum = 0;
+
+  for ( var i = 0; i < numbers.length; i++) {
+    sum += Math.pow(numbers[i], 2)
+    console.log(sum);
+  }
+  return sum;
+}
+
+alert(squareSum(arr));
 
