507c4d21b07991bafc31a841ab91e0b67ad989a3
greatorangejuice <bypavelsnigirev@gmail.com>
2018-10-17 21:39:09 +0300

    Task is Done*

--- CW9.js
+++ CW9.js
@@ -1,3 +1,20 @@
-function toWeirdCase() {
-    
+function toWeirdCase(string) {
+    let arr = string.split(" "); 
+    console.log(arr.length); 
+    let newString = ""; 
+
+    for ( let i = 0; i < arr.length; i++ ) {
+        let word = arr[i];
+        for ( let j = 0; j < word.length; j++ ) {  
+            if ( j % 2 == 0 ) {
+                newString += word.charAt(j).toUpperCase();
+            } else {
+                newString += word.charAt(j).toLowerCase();
+            } 
+        }
+        if ( i < arr.length - 1 ) newString += " ";
+    }   
+    return newString; 
 }
+
+alert( toWeirdCase("String case level pow") );
