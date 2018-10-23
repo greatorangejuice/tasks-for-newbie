b0a5db3353819886ca1660a5f0b8a1800216ffbb
Pavel Snigirev <43197939+greatorangejuice@users.noreply.github.com>
2018-10-23 11:07:10 +0300

    new Task

---
+++ CW12.js
@@ -0,0 +1,15 @@
+var countBits = function(n) {
+  // Program Me
+};
+
+
+/*
+Test.assertEquals(countBits(0), 0);
+Test.assertEquals(countBits(4), 1);
+Test.assertEquals(countBits(7), 3);
+Test.assertEquals(countBits(9), 2);
+Test.assertEquals(countBits(10), 2);
+*/
+
+// Example: The binary representation of 1234 is 10011010010,
+// so the function should return 5 in this case
