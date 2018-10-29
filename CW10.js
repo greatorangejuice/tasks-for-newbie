5978a8da96cc0751db018240fedef331236d0d84
greatorangejuice <bypavelsnigirev@gmail.com>
2018-10-30 00:31:35 +0300

    Task is done

--- CW10.js
+++ CW10.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-// function disemvowel(str) {
-//     return str;
-//   }
-=======
+
 function disemvowel(str) {
 let vowels = ["a", "i", "o", "u", "e"];
 let words = str.split("");
@@ -16,37 +12,3 @@ let newStr = "";
   }
   return newStr;
 }
->>>>>>> 680a59f83018739da26185f3c45af02a571ca018
-
-
-
-
-<<<<<<< HEAD
-//   Test.assertEquals(disemvowel("This website is for losers LOL!"),
-//   "Ths wbst s fr lsrs LL!")
-
-
-
-// Trolls are attacking your comment section!
-
-// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
-
-// Your task is to write a function that takes a string and return a new string with all vowels removed.
-
-// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
-
-// Note: for this kata y isn't considered a vowel.
-
-undefined = true; // устанавливаем мину для другого кода! остерегайтесь!
-
-(function IIFE( undefined ){
-
-	var a;
-	if (a === undefined) {
-		console.log( "Undefined is safe here!" );
-	}
-
-})();
-=======
-
->>>>>>> 680a59f83018739da26185f3c45af02a571ca018
