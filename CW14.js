61fefb390ac821f5241765e6e67ae5bf83ea8dd1
greatorangejuice <bypavelsnigirev@gmail.com>
2018-10-31 20:28:10 +0300

    Task is done

--- CW14.js
+++ CW14.js
@@ -1,25 +1,38 @@
-function incrementString (strng) {
-  // return incrementedString
+function incrementString (string) {
+  var symbol = string.split("");
+  var array = [];
+  var number = "";
+  var newNumber;
+  var incrementedString;
+
+for (let i = 0; i < symbol.length; i++) {
+  if (isNaN(symbol[i]) || symbol[i] == 0 ) {
+    array.push(symbol[i]);
+    console.log("Массив строк и нулей: " + array[i]);
+  } else {
+    number +=symbol[i];
+    console.log("Число: " + number);
+  }
 }
 
-Test.assertEquals(incrementString("foobar000"), "foobar001");
-Test.assertEquals(incrementString("foo"), "foo1");
-Test.assertEquals(incrementString("foobar001"), "foobar002");
-Test.assertEquals(incrementString("foobar99"), "foobar100");
-Test.assertEquals(incrementString("foobar099"), "foobar100");
-Test.assertEquals(incrementString(""), "1");
+if (number.length == 0 & array[array.length - 1] != 0 ) {
+  incrementedString = array.join("") + 1;
+} else {
+  newNumber = +number +1;
+  var newNumberStr = "" + newNumber;
+  console.log("Длина нового числа: " + newNumberStr.length);
+  console.log("Длина старого числа: " + number.length);
 
-
-foo -> foo1
-
-foobar23 -> foobar24
-
-foo0042 -> foo0043
-
-foo9 -> foo10
-
-foo099 -> foo100
-
-Ваша задача - написать функцию, которая увеличивает строку, чтобы создать новую строку.
-Если строка уже заканчивается номером, номер должен быть увеличен на 1.
-Если строка не заканчивается номером, номер 1 должен быть добавлен к новой строке.
+  if (newNumberStr.length == number.length) {
+    incrementedString = array.join("") + newNumberStr;
+  } else if ( newNumberStr.length != number.length & array[array.length - 1] == 0 ) {
+    array.pop();
+    console.log(array);
+    incrementedString = array.join("") + newNumberStr;
+  } else {
+    incrementedString = array.join("") + newNumberStr;
+  }
+}
+return incrementedString;
+}
+alert( incrementString("hello00") );
