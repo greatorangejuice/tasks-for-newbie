b7a91fb7315a46e2a7e7213654cb274bc097b450
Pavel Snigirev <43197939+greatorangejuice@users.noreply.github.com>
2018-11-02 11:22:10 +0300

    not all

--- CW15.js
+++ CW15.js
@@ -1,3 +1,25 @@
 function tickets(peopleInLine){
-    // ...
+ 
+  var moneyInCashBox = 25;
+  
+  if (peopleInLine[0] != 25) {
+      return "NO";
   }
+  
+  for ( let i = 1; i < peopleInLine.length; i++ ) {
+      moneyInCashBox += 25;
+      console.log("До сдачи: " + moneyInCashBox)
+      
+      if ( peopleInLine[i] > 25 ) {
+          moneyInCashBox -= (peopleInLine[i] - 25);
+           console.log("После сдачи: " + moneyInCashBox)
+      }
+      if (moneyInCashBox < 0) {return "NO"}
+  }
+  console.log(peopleInLine);
+  if (moneyInCashBox > 0) {
+  return "YES";
+  } else {
+  return "NO";
+  }
+}
