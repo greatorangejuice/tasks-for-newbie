543789268f3e1b91ca06bd0f6b181d65746a5aa9
greatorangejuice <bypavelsnigirev@gmail.com>
2018-11-05 23:21:48 +0300

    recode CW17

---
+++ CW17 v1.js
@@ -0,0 +1,15 @@
+function domainName(url) {
+
+}
+
+
+
+
+
+
+domainName("http://google.com");
+domainName("www.google.com");
+domainName("https://google.com");
+domainName("http://www.google.com");
+domainName("398d6q9tt.co/index.php");
+domainName("https://www.ouly7ietp-k2g2w00okvo.io/warez/");
