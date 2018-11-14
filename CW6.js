b2d4782b1a28eabcd721a125e8a554252da6a727
Pavel Snigirev <43197939+greatorangejuice@users.noreply.github.com>
2018-11-14 12:47:40 +0300

    task added

--- CW6.js
+++ CW6.js
@@ -5,4 +5,8 @@ function titleCase(title, minorWords) {
  
 }
 alert ( titleCase("раз два три пять"));
-
+/*
+titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings'
+titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
+titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
+*/
