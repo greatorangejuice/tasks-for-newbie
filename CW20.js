509f640bbe0493c6ca64594dc4e9c97ef125febe
Pavel Snigirev <43197939+greatorangejuice@users.noreply.github.com>
2018-11-20 11:17:32 +0300

    new task

---
+++ CW20.js
@@ -0,0 +1,11 @@
+function firstNonRepeatingLetter(s) {
+  // Add your code here
+}
+
+Test.describe('Simple Tests', function() {
+  it('should handle simple tests', function() {
+    Test.assertEquals(firstNonRepeatingLetter('a'), 'a');
+    Test.assertEquals(firstNonRepeatingLetter('stress'), 't');
+    Test.assertEquals(firstNonRepeatingLetter('moonmen'), 'e');
+  });
+});
