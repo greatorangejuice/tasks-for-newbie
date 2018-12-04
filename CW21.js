d902933a7e0f9e08ff205e831a2f29946b597b12
greatorangejuice <bypavelsnigirev@gmail.com>
2018-12-04 23:08:21 +0300

    Cata 5

--- CW21.js
+++ CW21.js
@@ -1,8 +1,24 @@
-var moveZeros = function (arr) {
-  // TODO: Program me
-}
+function moveZeros(arr) {
+  let zeroArr = arr.filter(function(zero) {
+    return zero === 0;
+  })
 
-Test.assertEquals(
-  JSON.stringify(moveZeros([1,2,0,1,0,1,0,3,0,1])),
-  JSON.stringify([ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ])
-);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+  
+  arr = arr.concat(zeroArr);
+  return arr;
+}
+var arr = [1,1,10,5,0,2,0,5,5,0,0,5];
+alert( moveZeros() );
+
+
+
+// not mine
+var moveZeros = function (arr) {
+  return arr.filter(function(x) {return x !== 0}).concat(arr.filter(function(x) {return x === 0;}));
+}
